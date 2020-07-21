a16f47fd7aa6cdeb90e97c8dc2b5ad6f04abd278
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-21 20:09:52 +0530

    initial commit

---
+++ app/src/main/java/com/densoftida/celladent/Constants.java
@@ -0,0 +1,31 @@
+package com.densoftida.celladent;
+
+import android.graphics.Bitmap.CompressFormat;
+
+public class Constants {
+	public final static String BROADCAST_WIZARD = "ida_org_in_broadcast_wizard";
+	public final static String BROADCAST_RESPONSE_CODE = "ida_org_in_broadcast_response_code";
+	public final static String BROADCAST_URL_ACCESS = "ida_org_in_broadcast_url_access";
+	public final static String BROADCAST_DATA = "ida_org_in_broadcast_broadcastdata";
+	public static final String PROJECTLINK = "https://play.google.com/store/apps/details?id=com.nukleap.ida";
+	public static final int STATUS_OK = 200;
+	public static final int NO_INTERNET = 1000;
+	public final static String PM = "pm";
+	public final static String AM = "am";
+	public static final String NO_INTERNET_MSG = "Please check the internet connention";
+	public static final int REQUEST_CAMERA = 45;
+	public static final int REQUEST_CROP = 46;
+	public static final int REQUEST_GALLARY = 41;
+	public static final String FILE_NAME_THUMB = "_thumb_mydoctorpro.jpeg";
+	public static final String FILE_NAME_EXT = ".jpeg";
+	public static final String FILE_NAME_CURRENT = "current_mydoctorpro.jpeg";
+	public static final String FILE_NAME_CURRENT_CROP = "current_crop_ida.jpeg";
+	public static final String FILE_NAME_CURRENT_THUMB = "current_thumb_ida.jpeg";
+	public static final String FILE_TEMP_NAME = "temp_img_ida_";
+	public static final CompressFormat FILE_FORMAT = CompressFormat.JPEG;
+	public static final int FILE_FORMAT_QUALITY = 100;
+	public static final int FILE_FORMAT_QUALITY_THUMB = 100;
+	public static final String PACK_NAME = "com_ida_main_app";
+	public static final String currenturl = "http://m.celladent.com/";
+	//public static final String currenturl = "https://dentalcart.in";
+}
